802c9f60ae877167d5ed42c4bb51763cef698bbf
Eric Chen <ericc26@uci.edu>
2026-06-21 01:34:08 -0500

    uibuild

--- index.js
+++ index.js
@@ -1,106 +1,99 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
-
-
-const uri = "mongodb+srv://fullstack:pp@cluster0.k0chrxf.mongodb.net/?appName=Cluster0"
-
-morgan.token('body', (req,res) =>  JSON.stringify(req.body))
 
 app.use(express.static('dist'))
 app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = 
-[
-  { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+const url = `mongodb+srv://fullstack:${password}@cluster0.k0chrxf.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
 ]
 
-app.get("/api/persons", ((request, response) =>
-    {
-      response.json(persons)
-    }
-  )
-)
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
 
-app.get("/api/persons/:id", ((request, response) => 
-    {
-      const id = request.params.id
-      const person = persons.find((person) => id === person.id)
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
 
-      if (person) {
-        response.send(person)
-      } else {
-        response.status(404).end()
-      }
-    }
-))
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter(note => note.id !== id)
 
-app.get("/info", ((request, response) =>
-    {
-      response.send(
-`<p>Phonebook has info for ${persons.length} people</p>
-<p>${new Date().toString()}</p>`
-      )
-    }
-  )
-)
+  response.status(204).end()
+})
 
-app.delete("/api/persons/:id", ((request, response) => 
-  {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+  
 
-    response.status(204).end()
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
   }
-))
+})
 
-app.post("/api/persons", ((request, response) => 
-  {
-    const body = request.body
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
 
-    if(!body.name || !body.number) {
-      return response.status(400).json({error: 'content missing'})
-    }
+app.post('/api/notes', (request, response) => {
+  const body = request.body
 
-    if(persons.find((person) => person.name === body.name)){
-      return response.status(400).json({error: 'name exists'})
-    }
-
-
-    const newPerson = 
-    {
-      id: Math.round(Math.random() * 10000),
-      name: body.name || '',
-      number: body.number || ''
-    }
-
-    persons = persons.concat(newPerson)
-
-    response.json(newPerson)
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
   }
-))
 
-const PORT = 3001
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
